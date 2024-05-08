e24e0f1861737d7e2665ee47cc38a0a459d6492c
Adrian Chojnicki <adrian@chojnicki.info>
2024-05-08 16:33:11 +0100

    changes

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
-from adisconfig import adisconfig
-from log import Log
+from adistools.adisconfig import adisconfig
+from adistools.log import Log
 from pika import BlockingConnection, PlainCredentials, ConnectionParameters
 from json import loads, dumps
 from pprint import pprint
